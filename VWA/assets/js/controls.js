5bfd298d92015d0810a602ad39074682ac07bd8e
Julius <juliushussl@gmail.com>
2020-05-08 17:52:36 +0200

    Added OpenPull

---
+++ VWA/assets/js/controls.js
@@ -0,0 +1,17 @@
+var app = angular.module('app', []);
+app.controller('Controls', function($scope) {
+    $scope.MoveDistance = 10;
+    $scope.ControllerInterface = true;
+    
+    
+    $scope.MoveUp = function(){
+        send("G0 "+$scope.MoveDistance);
+    }
+    $scope.MoveDown = function(){
+        send("G0 "+$scope.MoveDistance*-1);
+    }
+    
+    $scope.Stop = function(){
+        send("S ");
+    }
+});
